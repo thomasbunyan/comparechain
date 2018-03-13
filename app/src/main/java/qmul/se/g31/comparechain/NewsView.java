dac483f1d7791c45ec4d224b95be5f232541db01
unknown <t.bunyan@se16.qmul.ac.uk>
2018-03-13 19:33:10 +0000

    Initial commit

---
+++ app/src/main/java/qmul/se/g31/comparechain/NewsView.java
@@ -0,0 +1,24 @@
+package qmul.se.g31.comparechain;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thomas on 12/03/2018.
+ */
+
+public class NewsView extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_news, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/qmul/se/g31/comparechain/PortfolioView.java
@@ -0,0 +1,24 @@
+package qmul.se.g31.comparechain;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thomas on 12/03/2018.
+ */
+
+public class PortfolioView extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_portfolio, container, false);
+        return view;
+    }
+}
