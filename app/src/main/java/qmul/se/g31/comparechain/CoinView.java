6a8f6f33084c52d53c0045a5fb4482aa0024fe98
unknown <t.bunyan@se16.qmul.ac.uk>
2018-03-20 16:58:34 +0000

    Seach bar implemented and working

--- app/src/main/java/qmul/se/g31/comparechain/CoinView.java
+++ app/src/main/java/qmul/se/g31/comparechain/CoinView.java
@@ -2,6 +2,8 @@ package qmul.se.g31.comparechain;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
@@ -13,9 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
@@ -42,6 +46,8 @@ public class CoinView extends Fragment {
         Repository repo = Repository.getInstance();
         ArrayList<Coin> coin = repo.getData();
 
+        searchBar();
+
         this.myList = (ListView) view.findViewById(R.id.coinListView);
         ListAdapter myAdapter = new CoinRowAdapter(myList.getContext(), coin);
         myList.setAdapter(myAdapter);
@@ -60,13 +66,24 @@ public class CoinView extends Fragment {
         return view;
     }
 
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.main_window_search, menu);
+    public void searchBar(){
+        final SearchView searchView = (SearchView) view.findViewById(R.id.searchBar);
 
-        MenuItem searchItem = menu.findItem(R.id.coinListView);
-        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
+
+        int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = (TextView) searchView.findViewById(id);
+        textView.setTextColor(Color.BLACK);
+        id = searchView.getContext().getResources().getIdentifier("android:id/search_button", null, null);
+        ImageView imgView = (ImageView) searchView.findViewById(id);
+        Drawable whiteIcon = imgView.getDrawable();
+        whiteIcon.setTint(Color.GRAY);
+        imgView.setImageDrawable(whiteIcon);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -76,22 +93,22 @@ public class CoinView extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String s) {
-                ArrayList<String> tempList = new ArrayList<String>();
-                ArrayList<String> items = new ArrayList<String>();
+                ArrayList<Coin> tempList = new ArrayList<Coin>();
+                ArrayList<Coin> coin = new ArrayList<Coin>();
 
                 Repository repo = Repository.getInstance();
                 ArrayList<Coin> coins = repo.getData();
                 for(int i = 0; i < coins.size(); i++){
-                    items.add(coins.get(i).getName());
+                    coin.add(coins.get(i));
                 }
 
-                for(String temp : items){
-                    if(temp.toLowerCase().contains(s.toLowerCase())){
+                for(Coin temp : coin){
+                    if(temp.getName().toLowerCase().contains(s.toLowerCase()) || temp.getSymbol().toUpperCase().contains(s.toUpperCase())){
                         tempList.add(temp);
                     }
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, items);
-                myList.setAdapter(adapter);
+                ListAdapter myAdapter = new CoinRowAdapter(getContext(), tempList);
+                myList.setAdapter(myAdapter);
 
                 return true;
             }
--- app/src/main/java/qmul/se/g31/comparechain/MainWindowActivity.java
+++ app/src/main/java/qmul/se/g31/comparechain/MainWindowActivity.java
@@ -236,24 +236,24 @@ public class MainWindowActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_window_search, menu);
+        getMenuInflater().inflate(R.menu.main_window, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (true) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (true) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
