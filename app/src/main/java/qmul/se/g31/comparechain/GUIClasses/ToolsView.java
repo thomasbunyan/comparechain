d1674be45cd5e59df8ddb820bf5cf6fc96bfa9b8
unknown <t.bunyan@se16.qmul.ac.uk>
2018-03-22 12:42:50 +0000

    Added the comparison from Asim

--- app/src/main/java/qmul/se/g31/comparechain/GUIClasses/ToolsView.java
+++ app/src/main/java/qmul/se/g31/comparechain/GUIClasses/ToolsView.java
@@ -6,7 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+import qmul.se.g31.comparechain.DataClasses.Coin;
+import qmul.se.g31.comparechain.DataClasses.Favorites;
+import qmul.se.g31.comparechain.DataClasses.Repository;
 import qmul.se.g31.comparechain.R;
 
 /**
@@ -16,12 +27,127 @@ import qmul.se.g31.comparechain.R;
 public class ToolsView extends Fragment {
 
     View view;
+    private Repository repo;
+    private Coin coin1;
+    private Coin coin2;
+
+    private Spinner spinnerOne;
+    private Spinner spinnerTwo;
+
+    private TextView priceA;
+    private TextView marketcapA;
+    private TextView rankA;
+    private TextView percent1HA;
+    private TextView percent24HA;
+    private TextView percent7DA;
+
+    private TextView priceB;
+    private TextView marketcapB;
+    private TextView rankB;
+    private TextView percent1HB;
+    private TextView percent24HB;
+    private TextView percent7DB;
+
+    private TextView priceDiff;
+    private TextView mcapDiff;
+    private TextView rankDiff;
+    private TextView p1HDiff;
+    private TextView p24HDiff;
+    private TextView p7DDiff;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_tools, container, false);
+
+        repo = repo.getInstance();
+        Favorites fav = Favorites.getInstance();
+        ArrayList<Coin> coin = fav.getFavorites();
+        final ArrayList<String> coinNames = new ArrayList<String>();
+        for(int i = 0; i < coin.size(); i++) coinNames.add(coin.get(i).getName());
+
+        priceA = (TextView) view.findViewById(R.id.priceA);
+        marketcapA = (TextView) view.findViewById(R.id.marketcapA);
+        rankA = (TextView) view.findViewById(R.id.rankA);
+        percent1HA = (TextView) view.findViewById(R.id.percent1HA);
+        percent24HA = (TextView) view.findViewById(R.id.percent24HA);
+        percent7DA = (TextView) view.findViewById(R.id.percent7DA);
+
+        priceB = (TextView) view.findViewById(R.id.priceB);
+        marketcapB = (TextView) view.findViewById(R.id.marketcapB);
+        rankB = (TextView) view.findViewById(R.id.rankB);
+        percent1HB = (TextView) view.findViewById(R.id.percent1HB);
+        percent24HB = (TextView) view.findViewById(R.id.percent24HB);
+        percent7DB = (TextView) view.findViewById(R.id.percent7DB);
+
+        priceDiff = (TextView) view.findViewById(R.id.priceDiff);
+        mcapDiff = (TextView) view.findViewById(R.id.mcapDiff);
+        rankDiff = (TextView) view.findViewById(R.id.rankDiff);
+        p1HDiff = (TextView) view.findViewById(R.id.p1HDiff);
+        p24HDiff = (TextView) view.findViewById(R.id.p24HDiff);
+        p7DDiff = (TextView) view.findViewById(R.id.p7DDiff);
+
+
+        spinnerOne = (Spinner) view.findViewById(R.id.coinOne);
+        spinnerTwo = (Spinner) view.findViewById(R.id.coinTwo);
+        ArrayAdapter<String> a = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, coinNames);
+        a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerOne.setAdapter(a);
+        spinnerOne.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        NumberFormat priceFormatter = new DecimalFormat("#,###.00");
+                        coin1 = repo.searchCoin(coinNames.get(i));
+                        priceA.setText("$" + priceFormatter.format(coin1.getPrice()));
+                        marketcapA.setText("$" + priceFormatter.format(coin1.getMarketCap()));
+                        rankA.setText("#" + coin1.getRank());
+                        percent1HA.setText(priceFormatter.format(coin1.getPercent1h()) + "%");
+                        percent24HA.setText(priceFormatter.format(coin1.getPercent24h()) + "%");
+                        percent7DA.setText(priceFormatter.format(coin1.getPercent7d()) + "%");
+                        if(coin2 != null) calcDiff();
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {}
+                }
+        );
+
+        spinnerTwo.setAdapter(a);
+        spinnerTwo.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        NumberFormat priceFormatter = new DecimalFormat("#,###.00");
+                        coin2 = repo.searchCoin(coinNames.get(i));
+                        priceB.setText("$" + priceFormatter.format(coin2.getPrice()));
+                        marketcapB.setText("$" + priceFormatter.format(coin2.getMarketCap()));
+                        rankB.setText("#" + coin2.getRank());
+                        percent1HB.setText(priceFormatter.format(coin2.getPercent1h()) + "%");
+                        percent24HB.setText(priceFormatter.format(coin2.getPercent24h()) + "%");
+                        percent7DB.setText(priceFormatter.format(coin2.getPercent7d()) + "%");
+                        calcDiff();
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {}
+                }
+        );
+
         return view;
+    }
+
+    private void calcDiff(){
+        coin1 = repo.searchCoin(coin1.getSymbol());
+        coin2 = repo.searchCoin(coin2.getSymbol());
+
+        NumberFormat percentFormatter = new DecimalFormat("#,###");
+
+        priceDiff.setText("Price Difference: " + percentFormatter.format(((coin2.getPrice() - coin1.getPrice()) / coin2.getPrice()) * 100) + "%");
+        mcapDiff.setText("Marketcap Difference: " + percentFormatter.format(((coin2.getMarketCap() - coin1.getMarketCap()) / coin2.getMarketCap()) * 100) + "%");
+        rankDiff.setText("Rank Difference: " + percentFormatter.format(((coin2.getRank() - coin1.getRank()) / coin2.getRank()) * 100) + "%");
+        p1HDiff.setText("Percent1H Difference: " + percentFormatter.format(((coin2.getPercent1h() - coin1.getPercent1h()) / coin2.getPercent1h()) * 100) + "%");
+        p24HDiff.setText("Percent24H Difference: " + percentFormatter.format(((coin2.getPercent24h() - coin1.getPercent24h()) / coin2.getPercent24h()) * 100) + "%");
+        p7DDiff.setText("Percent7D Difference: " + percentFormatter.format(((coin2.getPercent7d() - coin1.getPercent7d()) / coin2.getPercent7d()) * 100) + "%");
     }
 
 
